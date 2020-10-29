41bf7d269d49260ec56fe359bc885fbd2880da4b
Gabriel <joaogabrielonofre@gmail.com>
2020-10-29 14:50:19 -0300

    Exercises 2.1 to 2.5 of part2

---
+++ course-information2/src/components/Course.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Sum = ({parts}) => {
+  const sum = parts.reduce((total, part) => total += part.exercises, 0)
+
+  return <p>total of exercises: {sum}</p>
+}
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({courseContent}) => {
+	return (
+	<>
+	{courseContent.map((part) => 
+		<Part key={part.id} part={part} />
+	)}
+	</>
+	)
+}
+
+const Header = ({courseName}) => <h1>{courseName}</h1>
+
+const Course = ({course}) => {
+	return (
+    <div>
+      <Header courseName={course.name} />
+	    <Content courseContent={course.parts} />
+      <Sum parts={course.parts} />
+    </div>
+		
+	)
+}
+
+export default Course

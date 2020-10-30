6b977c3096d903a43c76ba1904e3521871f68a3b
Gabriel <joaogabrielonofre@gmail.com>
2020-10-29 22:50:22 -0300

    Finish exercises 2.6 - 2.10 of part2

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,43 +1,77 @@
 import React, { useState } from 'react'
+import Form from './components/Form'
 
-const Person = ({person}) => <p>{person.name}</p>
+const Person = ({person}) => <p>{person.name} - {person.phone}</p>
+const Filter = ({search, handleSearch}) => <div>search person: <input type="text" value={search} onChange={handleSearch}/></div>
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone] = useState('')
+  const [search, setSearch] = useState('')
+  const [searchedPersons, setSearchedPersons] = useState([])
 
   const handleNameInput = (e) => {
-	setNewName(e.target.value)	
+	  setNewName(e.target.value)	
+  }
+
+  const handlePhoneInput = (e) => {
+    setNewPhone(e.target.value)
+  }
+
+  const clearForm = () => {
+    setNewName('')
+    setNewPhone('')
+    setSearch('')
+  }
+
+  const handleSearch = (e) => {
+    let q = e.target.value
+    setSearch(q)
+    const searchResult = persons.filter(person => {
+      return person.name.toLowerCase().includes(q.toLowerCase())
+    })
+    setSearchedPersons(searchResult)
   }
 
   const handleSubmit = (e) => {
-	e.preventDefault()
-	const newPerson = {
-		name: newName,
-    }
-    if(persons.find(person => person.name === newPerson.newName)){
-        alert(`${newName} is already on the list!`)
-    }
-	setPersons(persons.concat(newPerson))
-	setNewName('')	
+    e.preventDefault()
+    const newPerson = {
+      name: newName,
+      phone: newPhone,
+      }
+      if(persons.find((person) => person.name === newPerson.name)){
+          alert(`${newName} is already on the list!`)
+          clearForm()
+          return null
+      }
+    setPersons(persons.concat(newPerson))
+    clearForm()
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input type='text' value={newName} onChange={handleNameInput}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h1>Phonebook</h1>
+      <Filter search={search} handleSearch={handleSearch} />
+      <h2>Add a New Person</h2>
+      <Form 
+        handleSubmit={handleSubmit} 
+        newName={newName} 
+        newPhone={newPhone} 
+        handleNameInput={handleNameInput} 
+        handlePhoneInput={handlePhoneInput}
+      />
       <h2>Numbers</h2>
       <div>
-	    {persons.map((person) => <Person key={person.name} person={person} />)}
+      {searchedPersons.length 
+        ? searchedPersons.map((person) => <Person key={person.name} person={person} />)
+        : persons.map((person) => <Person key={person.name} person={person} />)
+      }
       </div>
     </div>
   )

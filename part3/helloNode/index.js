7569c765aaf4ebc122e85867b449deffb055ac39
Gabriel <joaogabrielonofre@gmail.com>
2020-11-04 11:59:05 -0300

    Deploy examples of part2 and part3 combined

--- part3/helloNode/index.js
+++ part3/helloNode/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 //middlewares
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('data', (req) => {
     if(req.method === 'POST') return (JSON.stringify(req.body))
@@ -51,11 +52,6 @@ let notes = [
   ]
 
 //routes
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
@@ -109,7 +105,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })

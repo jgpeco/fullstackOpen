2d66f76c3d61e7b5224b4a216fde8c9b256eaa6d
Gabriel <joaogabrielonofre@gmail.com>
2020-11-05 14:52:57 -0300

    Update and Remove routes with db connection, note-app

--- part3/helloNode/index.js
+++ part3/helloNode/index.js
@@ -70,11 +70,10 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end)
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -93,6 +92,21 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNote => {
         response.json(savedNote)
     })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+     .then(updatedNote => {
+         response.json(updatedNote)
+     })
+      .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {

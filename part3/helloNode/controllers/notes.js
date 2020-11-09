5e78ffda72d52c6c8c867a229370993d0fbcf5e7
Gabriel <joaogabrielonofre@gmail.com>
2020-11-09 16:20:55 -0300

    Refactor promises of controllers in note-app to be async/await, finish integrationt tests

--- part3/helloNode/controllers/notes.js
+++ part3/helloNode/controllers/notes.js
@@ -6,25 +6,22 @@ notesRouter.get('/', async (request, response) => {
     response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-    .then(note => {
-        if(note){
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+         response.json(note)
+    } else {
+        response.status(404).end()
+    }
+
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-    .then(() => response.status(204).end)
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const note = new Note({
@@ -33,14 +30,12 @@ notesRouter.post('/', (request, response, next) => {
         date: new Date(),
     })
 
-    note
-     .save()
-     .then(savedNote => savedNote.toJSON())
-     .then(savedAndFormattedNote => response.json(savedAndFormattedNote))
-     .catch(error => next(error))
+    const savedNote = await note.save()
+    response.json(savedNote)
+
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', (request, response) => {
     const body = request.body
 
     const note = {
@@ -52,7 +47,6 @@ notesRouter.put('/:id', (request, response, next) => {
     .then(updatedNote => {
          response.json(updatedNote)
      })
-    .catch(error => next(error))
 })
 
 module.exports = notesRouter

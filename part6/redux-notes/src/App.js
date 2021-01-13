e0a3a35e9c23551ce1cbe6f09765f7d11b22899a
Gabriel <joaogabrielonofre@gmail.com>
2021-01-13 12:19:20 -0300

    Create form and state management of form states in redux-notes

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -19,12 +19,50 @@ store.dispatch({
   }
 })
 
+const generateId = () => Number((Math.random() * 1000000).toFixed(1))
+
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  }
+}
+
 const App = () => {
+  const addNote = (e) => {
+    e.preventDefault()
+    const content = e.target.note.value
+    e.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input type="text" name="note"/>
+        <button type="submit">Add Note</button>
+      </form>
       <ul>
         {store.getState().map(note => 
-          <li key={note.id}>
+          <li 
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}  
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}

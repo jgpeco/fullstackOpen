2b3bc3c7f47378df13faf4f354f285bbe549af4c
Gabriel <joaogabrielonofre@gmail.com>
2021-01-13 12:58:44 -0300

    Separate components into minor presentational and container components

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -1,41 +1,12 @@
 import React from 'react'
-import {
-  createNote, toggleImportanceOf
-} from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
-
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
-  const addNote = (e) => {
-    e.preventDefault()
-    const content = e.target.note.value
-    e.target.note.value = ''
-    dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input type="text" name="note"/>
-        <button type="submit">Add Note</button>
-      </form>
-      <ul>
-        {notes.map(note => 
-          <li 
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}  
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }

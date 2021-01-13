98a9ea1bfb19368ea452f0b76bd9e7dc43c36881
Gabriel <joaogabrielonofre@gmail.com>
2021-01-13 12:50:39 -0300

    Separate action creators, store, reducers and react components responsability with react-redux

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -1,54 +1,23 @@
-import { createStore } from 'redux'
-import noteReducer from './reducers/noteReducer'
+import React from 'react'
+import {
+  createNote, toggleImportanceOf
+} from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  }
-})
-
-store.dispatch({
-  type: 'TOGGLE_IMPORTANCE',
-  data: {
-    id: 2,
-  }
-})
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(1))
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    data: { id }
-  }
-}
 
 const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
   const addNote = (e) => {
     e.preventDefault()
     const content = e.target.note.value
     e.target.note.value = ''
-    store.dispatch(createNote(content))
+    dispatch(createNote(content))
   }
 
   const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
+    dispatch(toggleImportanceOf(id))
   }
 
   return (
@@ -58,7 +27,7 @@ const App = () => {
         <button type="submit">Add Note</button>
       </form>
       <ul>
-        {store.getState().map(note => 
+        {notes.map(note => 
           <li 
             key={note.id}
             onClick={() => toggleImportance(note.id)}  
--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 

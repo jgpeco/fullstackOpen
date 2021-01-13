57f5df0cb1d7789fac0fca1b0e8e33d4ca4c0b4c
Gabriel <joaogabrielonofre@gmail.com>
2021-01-13 10:53:20 -0300

    Create redux-notes app. Create first Reducer: noteReducer

---
+++ part6/redux-notes/src/App.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux'
+import noteReducer from '../reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are mad with actions',
+    important: false,
+    id: 2,
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  data: {
+    id: 2,
+  }
+})
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App;

c6e10a1914ffb9f693c2a38c82b1b8c913b21b66
Gabriel <joaogabrielonofre@gmail.com>
2021-01-18 15:28:50 -0300

    Exercise 6.19 - Anecdotes - Refactor AnecdoteList to use connect instead of useDispatch-useSelector

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,34 +1,20 @@
 import React from 'react'
 import { voteInAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 
-const AnecdoteList = () => {
-    const anecdotes = useSelector( ({ filter, anecdotes }) => {
-        if(anecdotes.length){
-            const anecdotesArr = anecdotes.sort((a, b) => {
-                if(a.votes < b.votes) return 1
-                if(a.votes > b.votes) return -1
-                return 0
-            })
-    
-            if(filter) return anecdotesArr.filter(a => a.content.includes(filter))
-            return anecdotesArr
-        }
-        return []
-    })
-    
-    const dispatch = useDispatch()
+const AnecdoteList = ({ anecdotes, voteInAnecdote, setNotification}) => {
+    const anecdotesArr = anecdotes
 
     const vote = async (id) => {
-        const anecdote = anecdotes.find(a => a.id === id)
-        dispatch(voteInAnecdote(anecdote))
-        dispatch(setNotification(`You voted for ${anecdote.content}`, 5000))
+        const anecdote = anecdotesArr.find(a => a.id === id)
+        voteInAnecdote(anecdote)
+        setNotification(`You voted for ${anecdote.content}`, 5000)
     }
 
     return (
         <>
-            {anecdotes.map(anecdote =>
+            {anecdotesArr.map(anecdote =>
                 <div key={anecdote.id}>
                 <div>
                     {anecdote.content}
@@ -43,4 +29,26 @@ const AnecdoteList = () => {
     )
 }
 
-export default AnecdoteList
+const mapStateToProps = ({ filter, anecdotes }) => {
+    if(anecdotes.length){
+        const anecdotesArr = anecdotes.sort((a, b) => {
+            if(a.votes < b.votes) return 1
+            if(a.votes > b.votes) return -1
+            return 0
+        })
+
+        if(filter) return { anecdotes: (anecdotesArr.filter(a => a.content.includes(filter)))}
+        return { anecdotes: anecdotesArr }
+    }
+    return { anecdotes: [] }
+}
+
+const mapDispatchToProps = {
+    voteInAnecdote,
+    setNotification,
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AnecdoteList)
